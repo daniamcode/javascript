923b06a098d81d6c4c8deabe3f7558d79aa03b8d
Dani Alcala <daniamcode@gmail.com>
2022-03-20 12:32:19 +0100

    add some DOM sauce

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,21 @@
+const container = document.getElementById('container')
+
+// -----------------------------------------------------------------------------------------------------------
+
+// add style
+
+const myFirstDiv = document.createElement('div')
+
+// we can add a class
+myFirstDiv.classList.add('myClass')
+// or we can add each property like that:
+myFirstDiv.style.width = "500px"
+
+container.appendChild(myFirstDiv)
+
+
+// -----------------------------------------------------------------------------------------------------------
+
+
 // do some addEventListener in plain vanilla JS
+
