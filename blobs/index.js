a730e287c80c3d6a359a5fcfc6c42876bc528e44
Dani Alcala <daniamcode@gmail.com>
2022-03-20 12:32:51 +0100

    redo blob file

--- blobs/index.js
+++ blobs/index.js
@@ -1,23 +1,33 @@
-// Data URLs
+container = document.getElementById('container')
+
+// -------------------------------------------------------------------------------------------------
+
+// Here we will work with FILES as origin
+
+// see index.html for the execution of this function (there's an onClick html method)
+function saveFile() {
+    console.log('d')
+    const myFirstFile = fileupload.files[0]
+    console.log(myFirstFile) // expected result: a File type
+
+    // we can send this file to the backend
+    // and we can also show a preview:
+    const myFirstImageElement = document.createElement('img')
+    myFirstImageElement.style.width = "500px"
+    myFirstImageElement.src = URL.createObjectURL(myFirstFile)
+    container.appendChild(myFirstImageElement)
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// Here we will work with Data URLs as origin
+
 // Data URLs allow content creators to embed small files inline in documents
 // They have 4 parts: data:[<mediatype>][;base64],<data>
 // 1) a prefix (data:)
 // 2) a MIME type such as 'image/jpeg'
 // 3) an optional base64 token if non-textual
 // 4) the data itself 
-
-// -------------------------------------------------------------------------------------------------
-
-// see index.html for the execution of this function
-function saveFile() {
-    console.log('d')
-    console.log(fileupload.files[0])
-    // we can send this file to the backend
-}
-
-// -------------------------------------------------------------------------------------------------
-
-container = document.getElementById('container')
 
 function dataURLtoBlob(dataUrl) {
     if (dataUrl) {
@@ -49,8 +59,21 @@ function handleFile(file) {
     console.log(croppedImage)
 }
 
+// operations
+const mySecondFile = 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7'
+handleFile(mySecondFile)
+
+// print
+const mySecondImageElement = document.createElement('img')
+mySecondImageElement.src = mySecondFile
+container.appendChild(mySecondImageElement)
+
+// -------------------------------------------------------------------------------------------------
+
+// here we try with plain text; it doesn't work for now
+
+
 // const myFile = 'data:text/plain;base64,SGVsbG8sIFdvcmxkIQ==' // dataURL format
-const myFile = 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFz ByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSp a/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJl ZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uis F81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PH hhx4dbgYKAAA7'
 
 // const message = "Hello there, see if this will work or not"
 // const messageBase64 = btoa(message)
@@ -58,11 +81,7 @@ const myFile = 'data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAw AAAC8
 // const myFile = `data:text/plain;base64,${messageBase64}`
 // console.log(myFile)
 
-handleFile(myFile)
-
-const myImage = document.createElement('img')
-myImage.src = myFile
-container.appendChild(myImage)
-
 // -------------------------------------------------------------------------------------------------
+
+// try with Blobs
 
